a5525bd73ce976104d61dd7003abb92f17a1536b
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-06-04 00:42:10 +0200

    added attended events list to user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @organized_events = current_user.created_events.all
+    @attended_events = current_user.attended_events.all.to_a
   end
   
 end
