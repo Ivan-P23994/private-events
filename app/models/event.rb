d6149456ca6013871b74e47a2f30dcf753ea7ae5
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-06-05 13:50:02 +0200

    added upcoming and past events functionality scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,11 @@ class Event < ApplicationRecord
   validates_inclusion_of :date, 
   in: Date.today..Date.today+99.years,
   message: "Must be set in the future"
+
+
+  scope :past_events, -> {where("created_at <= ?", Date.today)}
+  scope :upcoming_events, -> {where("created_at >= ?", Date.today)}
+
   
   belongs_to :creator, class_name: 'User'
   has_many :invitations, dependent: :destroy
