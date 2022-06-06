8d025f3e75810e2753cacd5c13daa97569a6005f
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-06-06 20:03:51 +0200

    flash notice sorted

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,6 +8,40 @@ module.exports = {
   ],
   theme: {
     extend: {
+      height: {
+        "1vh": "1vh",
+        "2vh": "2vh",
+        "4vh": "4vh",
+        "6vh": "6vh",
+        "8vh": "8vh",
+        "10vh": "10vh",
+        "20vh": "20vh",
+        "30vh": "30vh",
+        "40vh": "40vh",
+        "50vh": "50vh",
+        "60vh": "60vh",
+        "70vh": "70vh",
+        "80vh": "80vh",
+        "90vh": "90vh",
+        "100vh": "100vh",
+      },
+      width: {
+        "1vw": "1vw",
+        "2vw": "2vw",
+        "4vw": "4vw",
+        "6vw": "6vw",
+        "8vw": "8vw",
+        "10vw": "10vw",
+        "20vw": "20vw",
+        "30vw": "30vw",
+        "40vw": "40vw",
+        "50vw": "50vw",
+        "60vw": "60vw",
+        "70vw": "70vw",
+        "80vw": "80vw",
+        "90vw": "90vw",
+        "100vw": "100vw",
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
@@ -19,3 +53,4 @@ module.exports = {
     require('@tailwindcss/typography'),
   ]
 }
+
